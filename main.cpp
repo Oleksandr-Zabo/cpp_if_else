923303b6472d13a743bcaf5e8307d082b3e26baa
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-27 17:57:00 +0300

    weight<500 is worked

--- main.cpp
+++ main.cpp
@@ -3,10 +3,60 @@ using namespace std;
 
 int main()
 { 
-	int lenth_A_B, lenth_B_C, weight;
-	cout << "Enter" << endl;
+	int lenth_A_B, lenth_B_C,
+		weight, capacity, 
+		need_fuel, need_B_C;
+	capacity = 300;
+	cout << "Enter the distance "
+		<<"between A and B: ";
+	cin >> lenth_A_B;
 
-	cout << "Hello world" << endl;
+	cout << "Enter the distance "
+		<< "between B and C: ";
+	cin >> lenth_B_C;
+
+	cout << "Enter the weight "
+		<< "of the load: ";
+	cin >> weight;
+
+	if (weight < 500) {
+		if (lenth_A_B <= capacity && lenth_B_C <= capacity) {
+			need_B_C = lenth_B_C;
+
+			need_fuel = need_B_C - (capacity - lenth_A_B);
+			(need_fuel > 0) ? cout << "You should refuel: "<< need_fuel<<" litrs"<<endl : 
+				cout << "You don't need to refuel(0)\n";
+
+		}
+
+		else {
+			cout << "Flight is not possible"
+				<<" on the entered route"<<endl;
+		}
+	}
+
+	else if (weight < 1000) {
+		if (lenth_A_B <= capacity/4 || lenth_B_C <= capacity/4) {
+			need_B_C = lenth_B_C*4;
+
+			need_fuel = need_B_C - (capacity/4 - lenth_A_B*4);
+			(need_fuel > 0) ? cout << "You should refuel " << need_fuel << " litrs" << endl : 
+				cout << "You don't need to refuel(0)\n";
+
+		}
+
+		else {
+			cout << "Flight is not possible"
+				<< " on the entered route" << endl;
+		}
+	}
+
+	else {
+		cout << "The load is too heavy\n";
+	}
+
+
+
 	system("pause");
 	return 0;
 }
