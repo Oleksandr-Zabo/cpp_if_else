9f0bbfbf3dd47464fbea472d2b43b65693f14928
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-27 16:11:42 +0300

    ex.4 lab had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 { 
-	cout << "Hello world" << endl;
+	int num_1, num_2;
+	cout << "Enter first number: ";
+	cin >> num_1;
+	
+	cout << "Enter second number: ";
+	cin >> num_2;
+	if (num_1 != num_2) {
+		(num_1 > num_2) ? 
+			cout << num_2 << " " << num_1<<endl : 
+			cout << num_1 << " " << num_2<<endl;
+	}
+
 	system("pause");
 	return 0;
 }
