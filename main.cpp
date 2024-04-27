7dd34b68dbd7b6d7ce564d4d4e21c50202c71936
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-27 18:26:40 +0300

    add Table of values

--- main.cpp
+++ main.cpp
@@ -19,76 +19,105 @@ int main()
 		<< "of the load: ";
 	cin >> weight;
 
-	if (weight < 500) {
-		if (lenth_A_B <= capacity && lenth_B_C <= capacity) {
-			need_B_C = lenth_B_C;
+	/*	Table of values
+	* weight | lenth_A_B | lenth_B_C | need_fuel
+	* <0-----------------------------------------
+	*  450    |  100      |  250      | 50
+	*  600    |  30       |  50       | 20
+	*  1400   |  30       |  20       | 50
+	*  1600   |  30       |  20       | 150
+	* 2000+   -----------------------------------
+	*/
 
-			need_fuel = need_B_C - (capacity - lenth_A_B);
-			(need_fuel > 0) ? cout << "You should refuel: "<< need_fuel<<" litrs"<<endl : 
-				cout << "You don't need to refuel(0)\n";
 
+
+
+	
+
+	if (weight < 0 || lenth_A_B < 0 || lenth_B_C < 0) {
+		cout << "Incorrect data" << endl;
+	}
+	else {//weight > 0 || lenth_A_B > 0 || lenth_B_C > 0
+
+		if (weight < 500) {
+			if (lenth_A_B <= capacity && lenth_B_C <= capacity) {
+				//Flight is possible on the entered route
+				need_B_C = lenth_B_C;
+
+				need_fuel = need_B_C - (capacity - lenth_A_B);
+				(need_fuel > 0) ? cout << "You should refuel: " << need_fuel << " litrs" << endl :
+					cout << "You don't need to refuel(0)\n";
+
+			}
+
+			else {
+				cout << "Flight is not possible"
+					<< " on the entered route" << endl;
+			}
+		}
+
+		else if (weight < 1000) {
+			if ((lenth_A_B <= (capacity / 4)) && (lenth_B_C <= (capacity / 4))) {
+				//Flight is possible on the entered route
+				need_B_C = lenth_B_C * 4;
+
+
+				//fixed
+				need_fuel = need_B_C - (capacity - lenth_A_B * 4);
+				(need_fuel > 0) ? cout << "You should refuel " << need_fuel << " litrs" << endl :
+					cout << "You don't need to refuel(0)\n";
+
+			}
+
+			else {
+				cout << "Flight is not possible"
+					<< " on the entered route" << endl;
+			}
+		}
+
+		else if (weight < 1500) {
+			if ((lenth_A_B <= (capacity / 7)) && (lenth_B_C <= (capacity / 7))) {
+				//Flight is possible on the entered route
+				need_B_C = lenth_B_C * 7;
+
+				need_fuel = need_B_C - (capacity - lenth_A_B * 7);
+				(need_fuel > 0) ? cout << "You should refuel " << need_fuel << " litrs" << endl :
+					cout << "You don't need to refuel(0)\n";
+
+			}
+
+			else {
+				cout << "Flight is not possible"
+					<< " on the entered route" << endl;
+			}
+		}
+
+
+		else if (weight < 2000) {
+			if ((lenth_A_B <= (capacity / 9)) && (lenth_B_C <= (capacity / 9))) {
+				//Flight is possible on the entered route
+				need_B_C = lenth_B_C * 9;
+
+				need_fuel = need_B_C - (capacity - lenth_A_B * 9);
+				(need_fuel > 0) ? cout << "You should refuel " << need_fuel << " litrs" << endl :
+					cout << "You don't need to refuel(0)\n";
+
+			}
+
+			else {
+				cout << "Flight is not possible"
+					<< " on the entered route" << endl;
+			}
 		}
 
 		else {
-			cout << "Flight is not possible"
-				<<" on the entered route"<<endl;
+			cout << "The load is too heavy\n";
 		}
+
 	}
+		
 
-	else if (weight < 1000) {
-		if ((lenth_A_B <= (capacity/4)) && (lenth_B_C <= (capacity/4))) {
-			need_B_C = lenth_B_C*4;
-			
-
-			//fixed
-			need_fuel = need_B_C - (capacity - lenth_A_B*4);
-			(need_fuel > 0) ? cout << "You should refuel " << need_fuel << " litrs" << endl : 
-				cout << "You don't need to refuel(0)\n";
-
-		}
-
-		else {
-			cout << "Flight is not possible"
-				<< " on the entered route" << endl;
-		}
-	}
-
-	else if (weight < 1500) {
-		if ((lenth_A_B <= (capacity / 7)) && (lenth_B_C <= (capacity / 7))) {
-			need_B_C = lenth_B_C * 7;
-
-			need_fuel = need_B_C - (capacity - lenth_A_B * 7);
-			(need_fuel > 0) ? cout << "You should refuel " << need_fuel << " litrs" << endl :
-				cout << "You don't need to refuel(0)\n";
-
-		}
-
-		else {
-			cout << "Flight is not possible"
-				<< " on the entered route" << endl;
-		}
-	}
-
-
-	else if (weight < 2000) {
-		if ((lenth_A_B <= (capacity / 9)) && (lenth_B_C <= (capacity / 9))) {
-			need_B_C = lenth_B_C * 9;
-
-			need_fuel = need_B_C - (capacity - lenth_A_B * 9);
-			(need_fuel > 0) ? cout << "You should refuel " << need_fuel << " litrs" << endl :
-				cout << "You don't need to refuel(0)\n";
-
-		}
-
-		else {
-			cout << "Flight is not possible"
-				<< " on the entered route" << endl;
-		}
-	}
-
-	else {
-		cout << "The load is too heavy\n";
-	}
+	
 
 
 
