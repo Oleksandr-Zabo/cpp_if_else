6880919926af18ee489a03427ed4e84cb4334473
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-27 16:55:47 +0300

    adapted the algorithm

--- main.cpp
+++ main.cpp
@@ -4,53 +4,45 @@ using namespace std;
 int main()
 { 
 	//create numbers
-	int num_in_1, num_in_2,
-		num_in_3, num_in_4,
-		num_in_5, num_in_6,
-		num_in_7, Max_num;
+	int num_in,Max_num;
 
 	//input nubers
 
 	cout << "Enter number 1: ";
-	cin >> num_in_1;
+	cin >> num_in;
+	Max_num = num_in;
 
 	cout << "Enter number 2: ";
-	cin >> num_in_2;
+	cin >> num_in;
+	(Max_num>num_in) ? Max_num = Max_num:
+	Max_num = num_in;
 
 	cout << "Enter number 3: ";
-	cin >> num_in_3;
+	cin >> num_in;
+	(Max_num > num_in) ? Max_num = Max_num :
+		Max_num = num_in;
 
 	cout << "Enter number 4: ";
-	cin >> num_in_4;
+	cin >> num_in;
+	(Max_num > num_in) ? Max_num = Max_num :
+		Max_num = num_in;
 
 	cout << "Enter number 5: ";
-	cin >> num_in_5;
+	cin >> num_in;
+	(Max_num > num_in) ? Max_num = Max_num :
+		Max_num = num_in;
 
 	cout << "Enter number 6: ";
-	cin >> num_in_6;
+	cin >> num_in;
+	(Max_num > num_in) ? Max_num = Max_num :
+		Max_num = num_in;
 
 	cout << "Enter number 7: ";
-	cin >> num_in_7;
+	cin >> num_in;
+	(Max_num > num_in) ? Max_num = Max_num :
+		Max_num = num_in;
 
-	//find max number
 	
-	(num_in_1 > num_in_2) ? Max_num = num_in_1 :
-		Max_num = num_in_1;
-
-	(Max_num > num_in_3) ? Max_num = Max_num :
-		Max_num = num_in_3;
-
-	(Max_num > num_in_4) ? Max_num = Max_num :
-		Max_num = num_in_4;
-
-	(Max_num > num_in_5) ? Max_num = Max_num :
-		Max_num = num_in_5;
-
-	(Max_num > num_in_6) ? Max_num = Max_num :
-		Max_num = num_in_6;
-
-	(Max_num > num_in_7) ? Max_num = Max_num :
-		Max_num = num_in_7;
 
 	cout << "The Highest Number: " << Max_num
 		<< endl;
