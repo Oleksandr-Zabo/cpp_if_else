3fd26ebc78227162ad7a8ba62bd9ee8c8e666f36
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-27 16:36:37 +0300

    ex.2 hw had done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,33 @@
 using namespace std;
 
 int main()
-{ 
-	cout << "Hello world" << endl;
+{
+	int num_in,
+		digit_1, digit_2,
+		digit_3, digit_4;
+	cout << "Enter your number: ";
+	cin >> num_in;
+	if (num_in > 999 && num_in < 100000) {
+		digit_4 = num_in % 10;
+		num_in /= 10;
+
+		digit_3 = num_in % 10;
+		num_in /= 10;
+
+		digit_2 = num_in % 10;
+		num_in /= 10;
+
+		digit_1 = num_in % 10;
+
+		cout << digit_2 << digit_1 
+			<< digit_4 << digit_3 
+			<<endl;
+
+	}
+	else {
+		cout << "The number is not four digits\n";
+	}
+
 	system("pause");
 	return 0;
 }
