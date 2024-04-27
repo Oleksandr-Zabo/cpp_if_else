85d96bce1c0eba281cdcd87362e9647f23c5588f
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-27 15:48:26 +0300

    ex.1 lab had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 { 
-	cout << "Hello world" << endl;
+	int user_in;
+	cout << "Enter number: ";
+	cin >> user_in;
+
+	(user_in % 2 == 0) ? cout 
+		<< "Your number is even" << endl : cout 
+		<< "Your number is odd" << endl;
+
 	system("pause");
 	return 0;
 }
